258dd5eda4b925532651a289e2d48d796fe36a15
Stella Yen <yens386@newschool.edu>
2016-12-15 14:21:45 +0000

    javascript

--- javascriptcomp2/js/main.js
+++ javascriptcomp2/js/main.js
@@ -1,11 +1,11 @@
-for (var dots = 0; dots < 900; dots++) {
+for (var dots = 0; dots < 896; dots++) {
     $(".wrapper").append("<div class='triangle'></div>");
 }
 
 var counter = 0;
 
 $('.triangle').each(function() {
-    $(this).css("transform","rotate("+ (counter/2000)*360 + "deg)");
+    $(this).css("transform","rotate("+ (counter/2000)*300 + "deg)");
     counter++;
 });
 
